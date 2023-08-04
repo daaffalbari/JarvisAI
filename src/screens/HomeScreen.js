a6dd46fb8fa081a436dd3ae99ccb1ff60774bd4e
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2023-08-04 09:29:45 +0700

    create feature components

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,26 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, SafeAreaView, Image, ScrollView, Alert} from 'react-native';
+import React, {useState} from 'react';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+import Features from '../components/features';
 
 export default function HomeScreen() {
+  const [messages, setMessages] = useState([]);
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-white">
+      <SafeAreaView className="flex-1 flex mx-5">
+        <View className="flex-row justify-center mt-5">
+          <Image
+            source={require('../../assets/images/bot.png')}
+            style={{height: hp(15), width: hp(15)}}
+          />
+        </View>
+
+        {/* Feature */}
+        {messages.length > 0 ? <View></View> : <Features />}
+      </SafeAreaView>
     </View>
   );
 }
